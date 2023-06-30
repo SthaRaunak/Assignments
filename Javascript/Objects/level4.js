f1ef2c73e883e0d626388ca64b8dc9d77dc8dd3b
Raunak Shrestha <raunaknagendra@gmail.com>
2023-06-30 23:49:20 +0545

    pushing objects level 4 js answers again

--- Javascript/Objects/level4.js
+++ Javascript/Objects/level4.js
@@ -6,17 +6,45 @@ const obj = {
 
 //calculate the percentage he scored in the exams out of total 150 score
 
+let sum = 0;
+
+objValues = Object.values(obj);
+
+objValues.map((item) => {
+    sum = sum + item;
+})
+let percent = (sum * 100 / 150).toFixed(2) + "%";
+console.log(percent);
 
 
-const userDetails={
-    name: "kaylin",
-    maths: 23,
-    science: 35,
-    gk:30
-}
 
+//Generate the given output
 // {
 //     userName: 'kaylin',
 //     subjectCodes: ['M','S', 'G'],
 //     subjectScores: [23,35,30]
 // }
+
+const userDetails = {
+    name: 'kaylin',
+    maths: 23,
+    science: 35,
+    gk: 30,
+};
+
+// answer
+let subjectCodes = [];
+
+const subject = Object.keys(userDetails);
+subject.forEach((item) => {
+    if (item != 'name') {
+        subjectCodes.push(item.charAt(0).toUpperCase());
+    }
+});
+
+const object = {
+    userName: userDetails.name,
+    subjectCodes,
+    subjectScores: Object.values(userDetails).filter(Number),
+};
+console.log(object)
