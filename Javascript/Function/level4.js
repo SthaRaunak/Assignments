02b08ceae2c534bb71648b06cf2f85f8661955e1
Raunak Shrestha <raunaknagendra@gmail.com>
2023-06-30 23:24:18 +0545

    editing level4 function answers

--- Javascript/Function/level4.js
+++ Javascript/Function/level4.js
@@ -16,3 +16,27 @@ function calcPrice(dist, unitPrice) {
 }
 calcPrice(rideDetails.distance, rideDetails.unitKmPrice);
 console.log(TotalPrice);
+
+//Q2 write a js function that checks if the object has status success or not
+//should return true or false
+
+const status1 ={
+    status: 'onTheWay'
+}
+const status2 ={
+    status: 'success'
+}
+const checkIfSucces=(status)=>{
+if(status.status == 'success')
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+
+checkIfSucces(status1)  //should return false
+
+checkIfSucces(status2)  //should return true
